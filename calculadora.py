db6ddf9af03282d0ffa0bb7a874241ca174d1146
tonaMG360 <80483523+tonaMG360@users.noreply.github.com>
2021-10-17 14:18:24 -0700

    First Version
    Initial RS

---
+++ calculadora.py
@@ -0,0 +1,41 @@
+end = False
+
+print (""" ************ 
+            Calulator 
+            **********
+
+            Menu 
+
+            1) sum
+            2) Subtraction
+            3) Mutiply
+            4) Divide 
+            5) exit """)
+
+while not (end):
+    option= int (input("Select an Option"))
+    if (option==1):
+        value1=int(input("First number"))
+        value2=int(input("Second number"))
+        print("The result is:", value1+value2)
+
+    elif (option==2):
+        value1=int(input("First number"))
+        value2=int(input("Second number"))
+        print("The result is:", value1-value2)
+
+    elif (option ==3):
+        value1=int(input("First number"))
+        value2=int(input("Second number"))
+        print("The result is:", value1*value2)
+    
+    elif (option==4):
+        value1=int(input("First number"))
+        value2=int(input("Second number"))
+        print("The result is:", value1/value2)
+    
+    else:
+        print("Exiting the calculator")
+        end= True
+
+
